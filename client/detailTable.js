9982c8cf704ca96d2a09337f817a46b7d7868374
pstuart <pstuart2@gmail.com>
2015-03-04 13:43:21 -0600

    Final formatting.

--- client/detailTable.js
+++ client/detailTable.js
@@ -14,38 +14,47 @@ Template.detailTable.helpers({
 });
 
 Template.detailRow.helpers({
-	queueTime: function() {
-        if (this.EmailData.PopulationStartTime === null || this.EmailData.StartTime === null) {
-            return '';
-        }
+	queueTime: function () {
+		if (this.EmailData.PopulationStartTime === null || this.EmailData.StartTime === null) {
+			return '';
+		}
 		return moment.utc(moment(this.EmailData.PopulationStartTime).diff(moment(this.EmailData.StartTime))).format('HH:mm:ss');
 	},
-	populationTime: function() {
-        if (this.EmailData.PopulationEndTime === null || this.EmailData.PopulationStartTime === null) {
-            return '';
-        }
+	populationTime: function () {
+		if (this.EmailData.PopulationEndTime === null || this.EmailData.PopulationStartTime === null) {
+			return '';
+		}
 		return moment.utc(moment(this.EmailData.PopulationEndTime).diff(moment(this.EmailData.PopulationStartTime))).format('HH:mm:ss');
 	},
-	executionTime: function() {
-        if (this.EmailData.EmailEndTime === null || this.EmailData.EmailStartTime === null) {
-            return '';
-        }
+	executionTime: function () {
+		if (this.EmailData.EmailEndTime === null || this.EmailData.EmailStartTime === null) {
+			return '';
+		}
 		return moment.utc(moment(this.EmailData.EmailEndTime).diff(moment(this.EmailData.EmailStartTime))).format('HH:mm:ss');
 	},
-  emailsPerSecond: function() {
-      var time = this.EmailData.EmailEndTime === null ? new Date() : this.EmailData.EmailEndTime;
-      var exTimeInSeconds = moment(time).diff(moment(this.EmailData.EmailStartTime), 'seconds');
-      if (isNaN(exTimeInSeconds) || exTimeInSeconds <= 0) {
-          return 0;
-      }
 
-	    return Math.floor(this.EmailData.TotalEmails / exTimeInSeconds);
-  },
-  percentComplete: function() {
-      return (this.EmailData.TotalEmails / this.EmailData.RecipientCount  * 100).toFixed() + '%';
-  },
-  estimatedCompletion: function() {
-      var emailsLeft = this.EmailData.RecipientCount - this.EmailData.TotalEmails;
-      return emailsLeft;
-  }
+	myCalcs: function () {
+		var time = this.EmailData.EmailEndTime === null ? new Date() : this.EmailData.EmailEndTime;
+		var exTimeInSeconds = moment(time).diff(moment(this.EmailData.EmailStartTime), 'seconds');
+		var emailsPerSecond;
+		if (isNaN(exTimeInSeconds) || exTimeInSeconds <= 0) {
+			emailsPerSecond = 0;
+		} else {
+			emailsPerSecond = Math.floor(this.EmailData.TotalEmails / exTimeInSeconds);
+		}
+
+		return {
+			emailsPerSecond: emailsPerSecond,
+			percentComplete: (this.EmailData.TotalEmails / this.EmailData.RecipientCount * 100).toFixed() + '%',
+			emailsLeft: this.EmailData.RecipientCount - this.EmailData.TotalEmails
+		};
+	},
+	estimatedCompletion: function () {
+		var secondsLeft = this.emailsLeft / this.emailsPerSecond;
+		if (secondsLeft <= 0) {
+			return '';
+		}
+		
+		return moment().add(secondsLeft).fromNow(true);
+	}
 });
